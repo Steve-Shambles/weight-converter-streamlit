90073a927d517ed29b62a97d7ccb8ecdb0bc673c
Steve Shambles <steveshambles@protonmail.com>
2023-03-10 09:12:54 +0000

    Add files via upload

--- Weight_Converter-streamlit.py
+++ Weight_Converter-streamlit.py
@@ -11,26 +11,26 @@ def convert_weight(weight, unit):
     pounds = 0
     kg = 0
 
-    if unit == 'stones':
+    if unit == 'Stones':
         stones = weight
         pounds = weight * 14
         kg = weight * 6.35
-    elif unit == 'pounds':
+    elif unit == 'Pounds':
         stones = weight / 14
         pounds = weight
         kg = weight * 0.453592
-    elif unit == 'kgrams':
+    elif unit == 'KGrams':
         stones = weight / 6.35
         pounds = weight / 0.453592
         kg = weight
 
-    message = f'{round(kg, 2)} kgrams\n {round(stones, 2)} stones\n {round(pounds, 2)} pounds'
+    message = f'{round(kg, 2)}  KGrams\n {round(stones, 2)}  Stones\n {round(pounds, 2)}  Pounds'
     st.success(message)
 
 st.title('Weight Converter')
 
 weight = st.text_input('Enter weight:')
-unit = st.selectbox('Select unit:', ('stones', 'pounds', 'kgrams'))
+unit = st.selectbox('Select unit:', ('Stones', 'Pounds', 'KGrams'))
 
 if st.button('Convert'):
     convert_weight(weight, unit)
